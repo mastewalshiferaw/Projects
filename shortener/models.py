3f75583fd6fb0c5c8df8596d2d392cb87de0c907
Mastewal <mastewalgshiferaw@gmail.com>
2026-05-03 22:48:15 +0300

    feat: implement unique short_code generation logic in model save method

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+import string
+import random
 
 class ShortenedURL(models.Model):
     original_url = models.URLField()
     short_code = models.CharField(max_length=10, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        #short code hasn't been set yet, then
+        if not self.short_code:
+            self.short_code = self.generate_unique_code()
+        super().save(*args, **kwargs)
+    
+    def generate_unique_code(self):
+        chars = string.ascii_letters + string.digits
+        while True:
+            new_code = ''.join(random.choice(chars) for _ in range(6))
+
+            #if it exits
+
+            if not ShortenedURL.objects.filter(short_code=new_code).exists():
+                return new_code
