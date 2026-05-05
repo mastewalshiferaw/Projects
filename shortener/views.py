205e0e22c7382508c2ad36d6583d0e4b854728d1
Mastewal <mastewalgshiferaw@gmail.com>
2026-05-05 22:43:14 +0300

    added views and forms to show the logic

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import ShortenedURL
 
-# Create your views here.
+def redirect_url_view(request, short_code):
+
+    obj = get_object_or_404(ShortenedURL, short_code=short_code)
+    return redirect(obj.original_url)
+
