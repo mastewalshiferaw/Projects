88a5cfe823e80ba1b759d034b2be16a19127ec81
Mastewal <mastewalgshiferaw@gmail.com>
2026-05-13 22:04:27 +0300

    added session based history tracker

--- shortener/views.py
+++ shortener/views.py
@@ -10,23 +10,32 @@ def redirect_url_view(request, short_code):
 
     obj.save()
 
-    
+
     return redirect(obj.original_url)
 
 
 def home_view(request):
     form = ShortenerForm(request.POST or None)
     new_url = None
+
+    recent_ids = request.session.get('recent_links', [])
     
     if request.method == "POST":
         if form.is_valid():
             
             obj = form.save()
+
+            recent_ids.append(obj.id)
+            request.session['recent_links']=recent_ids[-5:]
+
+
+
         
             new_url = request.build_absolute_uri('/') + obj.short_code
+    recent_links = ShortenedURL.objects.filter(id__in=recent_ids).order_by('-created_at')
             
     context = {
-        'form': form,
-        'new_url': new_url
+            'form': form,
+            'new_url': new_url
     }
     return render(request, 'shortener/home.html', context)
