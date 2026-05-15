1a654a55e3b4528547b1156e4f3613df4588225d
Mastewal <mastewalgshiferaw@gmail.com>
2026-05-15 22:46:03 +0300

    feat: implement session-based history to track recent links

--- shortener/views.py
+++ shortener/views.py
@@ -3,13 +3,11 @@ from .models import ShortenedURL
 from .forms import ShortenerForm
 
 def redirect_url_view(request, short_code):
-
-    obj=get_object_or_404(ShortenedURL, shortened_url=short_code)
+    
+    obj = get_object_or_404(ShortenedURL, short_code=short_code)
 
     obj.clicks += 1
-
     obj.save()
-
 
     return redirect(obj.original_url)
 
@@ -22,20 +20,18 @@ def home_view(request):
     
     if request.method == "POST":
         if form.is_valid():
-            
             obj = form.save()
 
             recent_ids.append(obj.id)
-            request.session['recent_links']=recent_ids[-5:]
-
-
-
+            request.session['recent_links'] = recent_ids[-5:]
         
             new_url = request.build_absolute_uri('/') + obj.short_code
+
     recent_links = ShortenedURL.objects.filter(id__in=recent_ids).order_by('-created_at')
             
     context = {
             'form': form,
-            'new_url': new_url
+            'new_url': new_url,
+            'recent_links': recent_links 
     }
     return render(request, 'shortener/home.html', context)
