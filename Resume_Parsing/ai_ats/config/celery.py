2685a39a6a990b9a4304d3fa1a5fb805f6ef82fc
Mastewal <mastewalgshiferaw@gmail.com>
2026-08-09 23:58:06 +0300

    setup for celery

---
+++ Resume_Parsing/ai_ats/config/celery.py
@@ -0,0 +1,15 @@
+# config/celery.py
+import os
+from celery import Celery
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('config')
+
+# - namespace='CELERY' means all celery-related configuration keys
+#   in settings.py should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
