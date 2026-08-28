d174289cf86f94e15070c7db6f7c306f6191dec5
Mastewal <mastewalgshiferaw@gmail.com>
2026-08-28 23:33:51 +0300

    fix: recruiter login in base.html

--- Resume_Parsing/ai_ats/config/urls.py
+++ Resume_Parsing/ai_ats/config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('kingsadd/', admin.site.urls),
 
-      path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/', include('django.contrib.auth.urls')), 
     
    
     path('', include('core.urls')), 
