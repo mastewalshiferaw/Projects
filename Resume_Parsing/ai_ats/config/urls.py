3b1ec18c8c505b343a7fae5a3d49430b006c8aab
Mastewal <mastewalgshiferaw@gmail.com>
2026-08-29 12:21:42 +0300

    added swagger ui endpoints

--- Resume_Parsing/ai_ats/config/urls.py
+++ Resume_Parsing/ai_ats/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('kingsadd/', admin.site.urls),
 
@@ -26,6 +26,15 @@ urlpatterns = [
     
    
     path('', include('core.urls')), 
+
+    # --API Authentication endpoints 
+    path('api/auth/', include('dj_rest_auth.urls')), 
+      path('api/auth/registration/', include('dj_rest_auth.registration.urls')), # Sign Up
+    
+    # SWAGGER UI ENDPOINTS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
 ]
 
 if settings.DEBUG:
