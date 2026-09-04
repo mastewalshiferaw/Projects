13d400a9308b56d842538d5b778270f3c00693bc
Mastewal <mastewalgshiferaw@gmail.com>
2026-09-04 13:13:54 +0300

    Fix: Opened ALLOWED_HOSTS for Render deployment

--- Resume_Parsing/ai_ats/config/settings.py
+++ Resume_Parsing/ai_ats/config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-7qg@)a(wawk^393aqul^go^4pt8h0ek%$gw0x5uekpw7!9r+b@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
